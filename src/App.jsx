10d3ce962e1bf4ca1743aa4c2304cb0cd0d4fc40
RifatParadoxical <zikkiz275@gmail.com>
2025-02-16 10:06:17 +0600

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,52 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+import { useForm } from 'react-hook-form'
 
 const App = () => {
-const [name, setName] = useState("")
-const [mail, setMail] = useState("")
-function handleSubmit(e) {
-  e.preventDefault();
-  console.log("Submitted:", {name, mail})
-  setName("")
-  setMail("")
-}
+ const { register, handleSubmit, watch,  formState:{errors} , reset} = useForm()
+ const [status, setStatus] = useState(null)
+ const password = watch("password", "")
+ const onSubmit = (data) =>{
+  setStatus(data)
+  reset()
+ }
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Enter Name' value={name} onChange={(e)=>{setName(e.target.value)}} />
-        <input type="email" placeholder='Enter Value' value={mail} onChange={(e)=>{setMail(e.target.value)}} />
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>Name: </label>
+        <input {...register('nickName' , {required: "Name is required", minLength:{value: 4, message:"Must be at least 4 characters"}, maxLength:{value: 10, message:"Must be under 10 characters"}})} type="text" placeholder='Enter your name'/>
+        {errors.nickName && <p style={{color: "red"}}>{errors.nickName.message}</p>}
+        </div>
+        <div>
+          <label>Email: </label>
+          <input {...register('email', {required: "Email is required"})} type="email" placeholder='Enter Your mail adress'/>
+          {errors.email && <p style={{color: "red"}}>{errors.email.message}</p>}
+        </div>
+        <div>
+          <label>Age: </label>
+          <input {...register('age', {required: "Age is required"})} type="number" placeholder='Enter Your Age'/>
+          {errors.age && <p style={{color:"red"}}>{errors.age.message}</p>}
+          {status && status.age < 18 && <p style={{color: "red"}}>You are not adult.</p>}
+        </div>
+        <div>
+          <label>Phone Number: </label>
+          <input {...register('contact', {required:"Phone Number is required"})} type="tel" placeholder='Enter Your phone number' />
+          {errors.contact && <p style={{color:"red"}}>{errors.contact.message}</p>}
+        </div>
+        <div>
+          <label>Password: </label>
+          <input {...register('password',{required:"Password is required", minLength:{value:6, message:"Must be at least 6 Characters"}})} type="password" placeholder='Enter Password' />
+          <p>Password Length: {password.length}</p>
+          {errors.password && <p style={{color:"red"}}>{errors.password.message}</p>}
+        </div>
         <button type='submit'>Submit</button>
       </form>
-    </div>
+        {status && (
+          <h1>Hello i'm  <strong>{status.nickName}</strong>. Subsribe your newsletter in my this mail adress: {status.email} and i ensure that i am {status.age} years old. For any further need don't hesitate to contact me on {status.contact}</h1>
+        )}
+    </>
   )
 }
 
 export default App
+
